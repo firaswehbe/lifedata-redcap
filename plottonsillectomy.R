c9890a950121e5ea1b89beb76009000850f8a38c
Firas Wehbe <firaswehbe@users.noreply.github.com>
2014-06-22 13:56:42 -0500

    some graph beautification and surgery indicator line

--- plottonsillectomy.R
+++ plottonsillectomy.R
@@ -2,7 +2,7 @@
 # Should be sourced after getredcap.R
 
 mytonsilDate <- as.Date("2014-06-11")
-mytonsilwindow <- c(mytonsilDate-30,mytonsilDate+30)
+mytonsilwindow <- c(mytonsilDate-1,mytonsilDate+20)
 mytonsilwindow <- as.POSIXct(mytonsilwindow)
 
 mytonsilweightsper <- data.frame(weights=myweights$weightvalue,timeof=myweights$timeoffact.timestamp)
@@ -12,6 +12,8 @@ mytonsilweightsper$weights <- mytonsilweightsper$weights / mybaselineweight * 10
 png("tonsillectomy.png",width=1200,height=900,res=150)
 plot(mytonsilweightsper$timeof,mytonsilweightsper$weights,
      ylim=c(80,100),xlim=mytonsilwindow,col="black",type="b",pch=18, lwd=2,
-     ylab="Weight (% of weight at surgery)")
+     ylab="Weight (% of weight at surgery)",xlab="")
 abline(h=seq(0,100,5),lty="solid",lwd=1) #150,250,10 in lbs
+abline(v=as.POSIXct("2014-06-11 12:00"),lwd=3)
+text(as.POSIXct("2014-06-11"),93,"Surgery",srt=90)
 dev.off()
